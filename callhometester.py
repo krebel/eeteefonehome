1f611fc480511d6b3bf2745188d654d8f111a0fa
krebel <lkrebelweb@gmail.com>
2017-08-26 20:02:18 -0400

    bug fixes

--- callhometester.py
+++ callhometester.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python
 #
-# Script to test access to the AutoSupport CallHome systems
+# Script to test access to the AutoSupport CallHome systems. (See KB's:
+# Veritas AutoSupport infrastructure update for Call Home endpoints
+# https://www.veritas.com/support/en_US/article.000126756
+#
+# Understanding the Call Home workflow for a NetBackup Appliance
+# https://www.veritas.com/support/en_US/article.000115419
+#
+# After upgrading to 2.7.3 Call Home test fails with error Failed to enable Call Home, and manually enabling Call Home fails.
+# https://www.veritas.com/support/en_US/article.000116140
+#
+# Important server change for NetBackup and NetBackup Appliance environments
+# https://www.veritas.com/support/en_US/article.INFO2803
+#
+# V-475-400-1000 How to troubleshoot Call Home on the NetBackup appliance?
+# https://www.veritas.com/support/en_US/article.000125447
+#
+# Callhome proxy enable fails with "unable to find valid certification path to requested target"
+# https://www.veritas.com/support/en_US/article.000107917
 #
 
 from __future__ import print_function
@@ -199,7 +216,7 @@ if __name__ == '__main__':
     selfname()
     printmac()
     hostschk()
-    resolv_chk
+    resolv_chk()
     ssl_ec2_test()
     ssl_yhoo_test()
     curltest()
